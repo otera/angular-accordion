f5d252432d50751fd1656fce469542884d9a3208
otera <capacityoverflow@reminiscence.sakura.ne.jp>
2020-07-06 15:30:15 +0900

    初期表示時にOpenできるよう値を追加

--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -9,23 +9,31 @@ export class BaseComponent implements OnInit {
   cities: {
     title: string;
     desc: string;
+    isOpen: boolean;
   }[] = [];
 
   constructor() {}
 
   ngOnInit(): void {
-    this.cities.push({ title: 'title', desc: 'A-HAHAHAHAHA!' });
+    this.cities.push({
+      title: 'title',
+      desc: 'A-HAHAHAHAHA!',
+      isOpen: false,
+    });
     this.cities.push({
       title: 'title2title2title2title2title2title2title2title2title2title2',
       desc: 'A-HAHAHAHAHA!!',
+      isOpen: false,
     });
     this.cities.push({
       title: 'title3 title3 title3 title3 title3 title3 title3 title3',
       desc: 'A-HAHAHAHAHA!<br>A-HAHAHAHAHA!!<br>A-HAHAHAHAHA!!!',
+      isOpen: true,
     });
     this.cities.push({
       title: 'title4',
       desc: 'Hello!<br>Hello!!<br>Hello!!!',
+      isOpen: false,
     });
   }
 }
